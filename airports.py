b0c27378ab010a6223d424ae24e96ba7a253dbe7
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-03-29 10:16:49 +0100

    commit_1

---
+++ airports.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import folium
+
+# Read data from Excel
+data = pd.read_excel('your_excel_file.xlsx')
+
+# Create a map
+mymap = folium.Map(location=[51.505, -0.09], zoom_start=3)
+
+# Iterate through the data and add markers
+for index, row in data.iterrows():
+    folium.Marker(location=[row['Latitude'], row['Longitude']], popup=row['Name']).add_to(mymap)
+
+# Save the map
+mymap.save('map_with_markers.html')
